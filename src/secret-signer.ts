78ac8a2aac1a676f93d781c4298bd35aa93fc914
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-10-28 00:00:25 +0800

    signer

--- src/secret-signer.ts
+++ src/secret-signer.ts
@@ -1,35 +1,31 @@
 // Adapted from: https://github.com/fastify/fastify-cookie/blob/3a9c1576bcf3f6d343c1a5b1792ce70234cd7ac1/signer.js
 
-import { sign, unsign } from "cookie-signature";
+import * as cookieSignature from "cookie-signature";
 import type { NormalizedOptions } from "./types";
+
+export function sign(value: string, options: NormalizedOptions): string {
+    return cookieSignature.sign(value, options.secret[0]);
+}
 
 type UnsignResult =
     | {
-          isValid: true;
-          isRotated: boolean;
+          valid: true;
+          rotated: boolean;
           value: string;
       }
     | {
-          isValid: false;
-          isRotated: boolean;
+          valid: false;
+          rotated: boolean;
           value: null;
       };
 
-export function createSecretSigner(options: NormalizedOptions) {
-    return {
-        sign(value: string): string {
-            return sign(value, options.secret[0]);
-        },
-
-        unsign(value: string): UnsignResult {
-            for (let i = 0; i < options.secret.length; ++i) {
-                const secret = options.secret[i];
-                const result = unsign(value, secret);
-                if (result !== false) {
-                    return { isValid: true, isRotated: i !== 0, value: result };
-                }
-            }
-            return { isValid: false, isRotated: false, value: null };
-        },
-    };
+export function unsign(value: string, options: NormalizedOptions): UnsignResult {
+    for (let i = 0; i < options.secret.length; ++i) {
+        const secret = options.secret[i];
+        const result = cookieSignature.unsign(value, secret);
+        if (result !== false) {
+            return { valid: true, rotated: i !== 0, value: result };
+        }
+    }
+    return { valid: false, rotated: false, value: null };
 }
